841c102c278cce40d26ce8938287841f41930a6e
qq986945193 <986945193@qq.com>
2018-01-08 14:52:19 +0800

    commit customer

--- emd_ssm_tools/src/main/java/com/david/emd_ssm_tools/demo1/web/controller/CustomerController.java
+++ emd_ssm_tools/src/main/java/com/david/emd_ssm_tools/demo1/web/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.david.emd_ssm_tools.demo1.web.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
